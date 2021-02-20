3bc4012c383222f1f2c01f3ed668f005e533f028
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-19 22:49:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ data=[[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
 open_or_senior(data)
 
 
-#someone else's much more efficient way
+#--------someone else's solution
 def openOrSenior(data):
   return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]
 
